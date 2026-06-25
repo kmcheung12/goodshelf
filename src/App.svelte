c9bfcf0dd6de1b2ffbbec9e5b6b5fa6084b79cbb
Alan <kmcheung12@gmail.com>
2026-06-25 08:35:05 +0100

    feat: scaffold Vite + Svelte + Three.js project

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import ShelfScene from './components/ShelfScene.svelte';
+</script>
+
+<ShelfScene />
---
+++ src/components/ShelfScene.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div id="canvas-container"></div>
+
+<style>
+  #canvas-container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
